a025318afc52590a4f5eef319fa144205da6b13a
OBaller <naseemoyebola@gmail.com>
2022-05-11 21:07:18 +0100

    initial commit

---
+++ Pagination/Pagination/PaginationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaginationApp.swift
+//  Pagination
+//
+//  Created by Naseem Oyebola on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct PaginationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
